10f9b356938c3eef70d635cd154d9a0ce73232ce
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-08-17 02:38:51 +0200

    skrócone zapisy warunków + ternary operator + zadanie do rozwiązania

--- src/pl/jaczewski/m3_operators/Operators.java
+++ src/pl/jaczewski/m3_operators/Operators.java
@@ -39,9 +39,10 @@ public class Operators {
         // Dwa znaki "==" to sprawdzenie, czy warunek jest spełniony
         // Znak "!=" sprawdza, czy coś nie równa się jakiejś wartości
 
+        //Oba warunki spełnione (&&) lub tylko jeden z nich (||)
         int firstScore = 25;
         int secondScore = 22;
-        if (firstScore > secondScore && firstScore > 20) {
+        if ((firstScore > secondScore) && (firstScore > 20)) {
             System.out.println("Pierwszy wynik jest większy od drugiego i jednocześnie większy od 20.");
         }
 
@@ -57,5 +58,40 @@ public class Operators {
             grade = '2';
         }
         System.out.println("Ocena końcowa to " + grade + ".");
+
+        boolean isCar = false;
+        if(!isCar){    // Tu sprawdzamy, czy isCar nie jest równy true (isCar != true)
+                        // (isCar == false) lub (isCar != true) lub (!isCar)
+                        // (isCar == true) lub po prostu (isCar)
+            System.out.println("Nie jest równy 'true'.");
+        }
+
+        //Ternary operator (potrójny operator)
+        boolean isNew = true;
+        boolean isItNew = isNew ? true : false;
+            // Pierwsza część (isNew) to coś, co chcemy zbadać;
+            // Druga część (true) to wartość, którą przypiszemy isItNew, jeśli badana rzecz (isNew) jest prawdą;
+            // Trzecia część (false) to wartość, która zostanie przypisana do isItNew, jeśli isNew jest fałszem.
+        if (isItNew) {
+            System.out.println("It is true");
+        }
+
+        boolean examPassed = true;
+        char wasItPassed = examPassed ? 'z' : 'n';
+        System.out.println(wasItPassed);
+
+        String didIMakeIt = (testPoints >= 60) ? "Egzamin zaliczony" : "Dwója! Wstyd!";
+        System.out.println(didIMakeIt);
+
+        // Zadanie
+        double firstDouble = 20.00;
+        double secondDouble = 80.00;
+        double sumOfDoublesMultiplied = (firstDouble + secondDouble) * 100;
+        double doublesRemainder = sumOfDoublesMultiplied % 40.00;
+        boolean isRemainderZero = (doublesRemainder == 0) ? true : false;
+        System.out.println(isRemainderZero);
+        if (!isRemainderZero) {
+            System.out.println("Got some remainder.");
+        }
     }
 }
