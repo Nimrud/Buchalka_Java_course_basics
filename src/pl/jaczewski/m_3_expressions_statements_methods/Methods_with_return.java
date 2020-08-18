9f44322e563ca22ce3ed5ea3f7a27fa54c2f2a0c
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-08-19 01:59:45 +0200

    metody - bez zwracania wartości i ze zwracaniem

---
+++ src/pl/jaczewski/m_3_expressions_statements_methods/Methods_with_return.java
@@ -0,0 +1,25 @@
+package pl.jaczewski.m_3_expressions_statements_methods;
+
+public class Methods_with_return {
+    public static void main(String[] args) {
+
+        calculateScore(true, 5000, 41, 100);
+
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    // Metoda ze zwracaniem wartości - w metodzie zamieniamy 'void' na typ zwracanych danych
+    // Do tego dodajemy 'return' ORAZ koniecznie, co zwrócić, jeśli warunek 'if' nie zostanie spełniony
+    public static int calculateScore(boolean gameOver, int score, int levelsCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelsCompleted * bonus);
+            finalScore += 5000;
+            System.out.println("Your final score is " + finalScore + ".");
+            return finalScore;
+        } else {
+            return -1;
+            // W programowaniu '-1' jest zwyczajowo używany, żeby zasygnalizować błąd
+            // a w algorytmach wyszukiwania '-1' oznacza nieprawidłową wartość lub wartość nieznalezioną
+        }
+    }
+}
