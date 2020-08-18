d8ae73d4b43e8b4c29af5f05dc918408e8fcf797
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-08-18 23:51:49 +0200

    expressions and statements

---
+++ src/pl/jaczewski/m_3_expressions_statements_methods/ExpressionsAndStatements.java
@@ -0,0 +1,15 @@
+package pl.jaczewski.m_3_expressions_statements_methods;
+
+public class ExpressionsAndStatements {
+    public static void main(String[] args) {
+
+        // expression = variables + values + operators (bez średnika na końcu!)
+                // kilometers = 100 * 1.609
+        // while (value < 50) { }  <= tu wszystko w nawiasie to expression
+        // przy metodach: wszystko w nawiasie to expression
+                // System.out.println("This is an expression");
+        // statement (deklaracja) = expression + data type + semicolon at end
+                // double kilometers = 100 * 1.609
+
+    }
+}
