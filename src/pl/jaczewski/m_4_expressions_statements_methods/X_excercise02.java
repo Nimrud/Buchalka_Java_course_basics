e6094fa414d24558e74c581c16b10e6718f4f64d
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-08-20 03:14:20 +0200

    metody - zadanie 02, 03, 04

---
+++ src/pl/jaczewski/m_4_expressions_statements_methods/X_excercise02.java
@@ -0,0 +1,14 @@
+package pl.jaczewski.m_4_expressions_statements_methods;
+
+public class X_excercise02 {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = kiloBytes / 1024;
+            int kiloBytesRemainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + kiloBytesRemainder + " KB");
+        }
+    }
+}
