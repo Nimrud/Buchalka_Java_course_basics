ee0c7a4dfad38319b7c58b5cebb2214dc064802e
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-08-22 06:32:20 +0200

    metody - zadanie 8 i 9

---
+++ src/pl/jaczewski/m_4_expressions_statements_methods/X_exercise09.java
@@ -0,0 +1,16 @@
+package pl.jaczewski.m_4_expressions_statements_methods;
+
+public class X_exercise09 {
+
+    public static void printYearsAndDays (long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long years = minutes / (365 * 24 * 60);
+            long minutesRemainder = minutes % (365 * 24 * 60);
+            long days = minutesRemainder / (24 * 60);
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
+
