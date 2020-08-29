eeef79242fb086c115cf8e8b308edc7253cba07a
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-08-29 02:34:52 +0200

    OOP: classes, constructors, inheritance: klasy (2 zadania)

---
+++ src/pl/jaczewski/m6_OOP_classes_constructors_inheritance/X_Main.java
@@ -0,0 +1,34 @@
+package pl.jaczewski.m6_OOP_classes_constructors_inheritance;
+
+public class X_Main {
+    public static void main(String[] args) {
+
+        // Zadanie 29
+        System.out.println("Zadanie 29:");
+        X_exercise29_SimpleCalculator calculator = new X_exercise29_SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+        System.out.println();
+
+        // Zadanie 30
+        System.out.println("Zadanie 30:");
+        X_exercise30_Person person = new X_exercise30_Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+    }
+}
