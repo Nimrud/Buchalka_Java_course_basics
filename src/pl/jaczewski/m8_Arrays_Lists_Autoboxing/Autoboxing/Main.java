ed2e3da6c1c666b95eaf242eed588392ead951ef
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-09-10 23:54:36 +0200

    Arrays_Lists_Autoboxing: Autoboxing challenge (w trakcie rozwiązywania (3))

--- src/pl/jaczewski/m8_Arrays_Lists_Autoboxing/Autoboxing/Main.java
+++ src/pl/jaczewski/m8_Arrays_Lists_Autoboxing/Autoboxing/Main.java
@@ -13,10 +13,95 @@ public class Main {
         boolean quit = false;
         printInstructions();
 
+        while(quit){
+            boolean isInt = sc.hasNextInt();
+            if (!isInt){
+                System.out.println("Invalid number. Try again.");
+                sc.nextLine();
+            } else {
+                option = sc.nextInt();
+                sc.nextLine();
+
+                switch (option){
+                    case 0:
+                        printInstructions();
+                        break;
+                    case 1:
+                        displayBranches();
+                        break;
+                    case 2:
+                        addNewBranch();
+                        break;
+                    case 3:
+                        modifyBranch();
+                        break;
+                    case 4:
+                        addCustomer();
+                        break;
+                    case 5:
+                        addTransaction();
+                        break;
+                    case 6:
+                        displayCustomersOfBranch();
+                        break;
+                    case 7:
+                        displayTransactionsOfBranch();
+                        break;
+                    case 8:
+                        modifyCustomerData();
+                        break;
+                    case 9:
+                        removeCustomer();
+                        break;
+                    case 10:
+                        findCustomer();
+                        break;
+                    case 11:
+                        quit = true;
+                        break;
+                    default:
+                        printInstructions();
+                        break;
+                }
+            }
+        }
+
     }
 
 
     public static void addCustomer(){
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+        System.out.print("Enter PESEL number: ");
+        int pesel = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter customer's deposit: ");
+        double amount = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Enter branch name: ");
+        String branchName = sc.nextLine();
+        bank.addCustomerToBranch(branchName, customerName, pesel, amount);
+    }
+
+    public static void displayBranches(){
+        bank.displayBranches();
+    }
+
+    public static void addNewBranch(){
+        System.out.print("Enter a name of the new branch: ");
+        String branchName = sc.nextLine();
+        bank.addBranch(branchName);
+    }
+
+    public static void modifyBranch(){
+        System.out.print("Enter an existing branch name: ");
+        String oldName = sc.nextLine();
+        System.out.print("Enter new branch name: ");
+        String newName = sc.nextLine();
+        bank.updateBranch(oldName, newName);
+    }
+
+    public static void findCustomer(){
 
     }
 
@@ -24,15 +109,16 @@ public class Main {
         System.out.println("Menu: ");
         System.out.println("\t 0 - To display options.");
         System.out.println("\t 1 - To display bank branches.");
-        System.out.println("\t 2 - To modify an existing branch.");
-        System.out.println("\t 3 - To add a new customer to a branch.");
-        System.out.println("\t 4 - To add a transaction for a customer.");
-        System.out.println("\t 5 - To display all customers of a branch.");
-        System.out.println("\t 6 - To display all transactions of customers of particular branch.");
-        System.out.println("\t 7 - To modify customer data.");
-        System.out.println("\t 8 - To remove a customer.");
-        System.out.println("\t 9 - To search for a customer.");
-        System.out.println("\t 10 - To quit the application.");
+        System.out.println("\t 2 - To add a new branch.");
+        System.out.println("\t 3 - To modify an existing branch.");
+        System.out.println("\t 4 - To add a new customer to a branch.");
+        System.out.println("\t 5 - To add a transaction for a customer.");
+        System.out.println("\t 6 - To display all customers of a branch.");
+        System.out.println("\t 7 - To display all transactions of customers of particular branch.");
+        System.out.println("\t 8 - To modify customer data.");
+        System.out.println("\t 9 - To remove a customer.");
+        System.out.println("\t 10 - To search for a customer.");
+        System.out.println("\t 11 - To quit the application.");
     }
 }
 
