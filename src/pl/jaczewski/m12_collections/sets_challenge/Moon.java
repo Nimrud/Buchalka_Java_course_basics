f0e80f03fd3931e2a9db50f2cf17ae9dac7b5418
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2020-11-18 23:12:39 +0100

    m12_collections: maps - Sets challenge (4)

---
+++ src/pl/jaczewski/m12_collections/sets_challenge/Moon.java
@@ -0,0 +1,8 @@
+package pl.jaczewski.m12_collections.sets_challenge;
+
+public class Moon extends HeavenlyBody{
+
+    public Moon(String name, double orbitalPeriod) {
+        super(name, BodyType.MOON, orbitalPeriod);
+    }
+}
