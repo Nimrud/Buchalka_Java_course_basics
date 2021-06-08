1f088b7dff7d5e664e3b39d4bf85e5e27e096ac1
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-06-08 19:58:50 +0200

    m13_extra_challenges (1)

---
+++ src/pl/jaczewski/m7_OOP_composition_encapsulation_polymorphism/new_composition_challenge/Wall.java
@@ -0,0 +1,13 @@
+package pl.jaczewski.m7_OOP_composition_encapsulation_polymorphism.new_composition_challenge;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
