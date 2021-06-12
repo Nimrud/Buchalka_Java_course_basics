cab5d05b9873866f6672d1ba3bc68fc3fcbafeb6
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-06-12 23:58:33 +0200

    m13_extra_challenges (7): Mobile Phone app (1)

---
+++ src/pl/jaczewski/m8_arrays_lists_autoboxing/new_challenges/mobile_phone/Contact.java
@@ -0,0 +1,23 @@
+package pl.jaczewski.m8_arrays_lists_autoboxing.new_challenges.mobile_phone;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+
+    }
+}
